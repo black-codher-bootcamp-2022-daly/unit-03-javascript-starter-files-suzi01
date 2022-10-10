895cace6f913aafed41b10d500290c9a77ecf742
suzi01 <suzannahthompson@hotmail.co.uk>
2022-10-10 19:19:37 +0100

    Variable exercise"

--- session-2/index.js
+++ session-2/index.js
@@ -1,2 +1,8 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+console.log('Hello there')
+
+const first = document.querySelector('.about').innerHTML
+console.log(first)
+
+const second = document.querySelectorAll()
