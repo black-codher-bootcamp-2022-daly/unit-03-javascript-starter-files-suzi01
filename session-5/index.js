2797d888835c30e7f2a01b6fdbec970683f208d0
suzi01 <suzannahthompson@hotmail.co.uk>
2022-10-17 20:58:51 +0100

    Switch statements

--- session-5/index.js
+++ session-5/index.js
@@ -26,3 +26,47 @@ getGrade(55)
 getGrade(42)
 getGrade(35)
 getGrade(28)
+
+
+
+function getGradeSwitch(total){
+    switch (true) {
+        case (total > 90):
+            console.log('A+')
+            break;
+        case (total >= 80):
+            console.log('A')
+            break;
+        case (total > 70):
+            console.log('B')
+            break;
+        case (total > 60):
+            console.log('C')
+            break;
+        case (total > 50):
+            console.log('D')
+            break; 
+        case (total > 40):
+            console.log('D')
+            break;
+        case (total > 30):
+            console.log('F')
+            break;
+
+                
+
+        default:
+            console.log("Fail")
+            break;
+    }
+}
+    
+
+getGradeSwitch(95) 
+getGradeSwitch(88)
+getGradeSwitch(76)
+getGradeSwitch(61)
+getGradeSwitch(55)
+getGradeSwitch(42)
+getGradeSwitch(35)
+getGradeSwitch(28)
